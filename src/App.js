5f69d7220c3872b1b7ce3ccfde4b08346558bbe4
Bryan Rojas <bryroj14@gmail.com>
2020-02-01 14:38:52 -0800

    Fixed app.js error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
+      <Router>
         <div className="App">
           <Navbar />
 
         </div>
+      </Router>
     )
   }
 }
