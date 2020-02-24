ce725a5fb18afe7dcc59b3395c64d67f5dbb9c98
Bryan Rojas <bryroj14@gmail.com>
2020-02-23 20:56:37 -0800

    Updated profile placeholder

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Header } from 'react-bootstrap/lib/Modal';
-import { Button } from 'react-bootstrap';
-
-
 
 class Profile extends Component {
     render() {
-      
-        <div className = "App">
-            <h1> REact with bootstrap</h1>
-            <Button bsStyle = "primary"></Button>
-
-        </div>
-      
+        return(
+            <div>
+                Profile 
+            </div>
+        )
     }
   }
   
-  export default App
+  export default Profile
