21a24d817e59354a2db1d88026bfb2f46581e3e4
Hector Mendoza <44214086+hmendoza23@users.noreply.github.com>
2020-03-09 15:20:17 -0700

    added links

--- src/App.js
+++ src/App.js
@@ -17,14 +17,11 @@ import Calculator from './components/Calculator/Calculator'
 import Access from './components/FooterLinks/Accessibility'
 import Cookies from './components/FooterLinks/Cookies'
 import Privacy from './components/FooterLinks/Privacy'
-<<<<<<< HEAD
 import Features from './components/FooterLinks/Features'
 import Goals from './components/FooterLinks/Goals'
 import BodyPicker from './components/BodyPicker/BodyPicker'
-=======
 import TOS from './components/FooterLinks/TermsOfService'
 import ForgotPass from './components/ForgotPassword'
->>>>>>> dd86be2cc2b8d42a8cf39c13e48d946567b828e5
 
 class App extends Component {
   render() {
