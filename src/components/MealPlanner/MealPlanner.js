9899bf16b16e2be4247c4da52627e578ca79a212
Bryan-Rojas <bryanrojascs@gmail.com>
2020-03-16 17:47:50 -0700

    Updated with meal planning

--- src/components/MealPlanner/MealPlanner.js
+++ src/components/MealPlanner/MealPlanner.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Results from './Results'
+
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://2o8jsg6z03.execute-api.us-west-1.amazonaws.com/default/'
 
 class MealPlanner extends Component {
   constructor() {
     super()
     this.state = {
-      diet_type: '',
+      diet_type: 'Any',
       calories: '',
-      errors: ''
+      errors: '',
+      results: ''
     }
 
     this.onChange = this.onChange.bind(this)
@@ -15,23 +20,63 @@ class MealPlanner extends Component {
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value })
+    console.log(this.state)
   }
 
   onSubmit(e) {
+    if(this.state.error !== ''){
+      this.setState({ errors: '' })
+    }
+
+    let current_calories = parseInt(this.state.calories)
+
+    if(this.state.calories === ''){
+      this.setState({ errors: 'Empty fields.' })
+    }
+    else if(current_calories < 800 | current_calories > 5200){
+      this.setState({ errors: 'Invalid amount of calories, please choose a value between 800 to 5200 calories.' })
+    }
+
+    let payload = {}
+    payload['request'] = {}
+    payload['request']['diet-type'] = this.state.diet_type
+    payload['request']['calories'] = this.state.calories
+
     e.preventDefault()
-    console.log(e)
+
+    axios.post('mealplanner', payload)
+    .then(response => {
+      this.setState({ results: JSON.stringify(response.data)})
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   render() {
     const errors = (
       <>
-        <div className="alert alert-danger" role="alert">
+        <div className="alert alert-danger" style={{marginLeft: '1em', marginRight: '1em'}} role="alert">
           <strong>Errors:</strong><br/>
             <ul>
-              <li>Invalid amount of calories, please choose a value between <strong>800</strong> to <strong>5200</strong> calories.</li>
+              <li>{this.state.errors}</li>
             </ul>
         </div>
       </>
+    )
+    
+    const results = (
+      <>
+        <Results diet={this.state.diet_type} calories={this.state.calories} results={this.state.results}/>
+      </>
+    )
+
+    const gen_meal_planner = (
+      <button type="submit" className="btn btn-primary"> <span role="img" aria-label="sandwich">🥪</span> Generate Meal Plan</button>
+    )
+
+    const regen_meal_planner = (
+      <button type="submit" className="btn btn-primary"> <span role="img" aria-label="redo">🔁</span> Regenerate Meal Plan</button>
     )
 
     return (
@@ -53,40 +98,42 @@ class MealPlanner extends Component {
 
                     <form onSubmit={this.onSubmit}>
 
-                    <center>
-                    <div className="form-group">
+                      <center>
+                      <div className="form-group">
 
-                    <div className="row">
-                      <div className="col-md-2" style={{marginTop: '0.5em', marginRight: '1em', marginLeft: '1em'}}>
-                        <label for="exampleFormControlSelect1">Diet Type</label>
+                      <div className="row">
+                        <div className="col-md-2" style={{marginTop: '0.5em', marginRight: '1em', marginLeft: '1em'}}>
+                          <label for="exampleFormControlSelect1">Diet Type</label>
+                        </div>
+                        <div className="col-md-8" style={{marginRight: '1em', marginLeft: '1em'}}>
+                          <select className="form-control" id="exampleFormControlSelect1" name="diet_type" value={this.state.diet_type} onChange={this.onChange}>
+                            <option>Any</option>
+                            <option>Vegan</option>
+                            <option>Vegetarian</option>
+                            <option>Paleo</option>
+                            <option>Ketogenic</option>
+                          </select>
+                        </div>
                       </div>
-                      <div className="col-md-8" style={{marginRight: '1em', marginLeft: '1em'}}>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                          <option>Any</option>
-                          <option>Vegan</option>
-                          <option>Vegetarian</option>
-                          <option>Paleo</option>
-                          <option>Ketogenic</option>
-                        </select>
+
+                      <br/>
+
+                      <div className="row">
+                        <div className="col-md-2" style={{marginTop: '0.5em', marginRight: '1em', marginLeft: '1em'}}>
+                          <label for="exampleFormControlSelect1">Calories</label>
+                        </div>
+                        <div className="col-md-8" style={{marginRight: '1em', marginLeft: '1em'}}>
+                          <input type="text" name="calories" value={this.state.calories} onChange={this.onChange} class="form-control" id="formGroupExampleInput" placeholder="Amount of calories desired. (800-5200)"/>
+                        </div>
                       </div>
-                    </div>
 
-                    <br/>
+                      <br/>
 
-                    <div className="row">
-                      <div className="col-md-2" style={{marginTop: '0.5em', marginRight: '1em', marginLeft: '1em'}}>
-                        <label for="exampleFormControlSelect1">Calories</label>
                       </div>
-                      <div className="col-md-8" style={{marginRight: '1em', marginLeft: '1em'}}>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Amount of calories desired. (400-5200)"/>
-                      </div>
-                    </div>
 
-                    <br/>
+                        {this.state.results === '' ? gen_meal_planner : regen_meal_planner}
 
-                    </div>
-                      <button type="submit" className="btn btn-primary"> <span role="img" aria-label="sandwich">🥪</span> Generate Meal Plan</button>
-                    </center>
+                      </center>
                
                     </form>
 
@@ -99,6 +146,11 @@ class MealPlanner extends Component {
             </div>
 
           </div>
+
+
+        
+        <br/>
+        {this.state.results === '' ? null : results}
 
         </div>
       </div>
---
+++ src/components/MealPlanner/Results.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import '../../static/css/Meal-Planner-Result.css'
+const Card = props =>{
+    let json_result = JSON.parse(props.results)
+    console.log(json_result)
+    return(
+        <div className="row">
+        <div className="col-md-1">
+        </div>
+
+        <div className="col-md-10" >
+            <div className="card" style={{width: '100%'}}>
+
+                <div className="card-header">
+                    <h3 style={{fontSize: '1.2em'}}>Meal Plan</h3>
+                </div>
+
+                <div className="card-body" style={{marginLeft: '3em', marginRight: '3em'}}>
+                    <center>
+                        <strong>Dietary Preference:</strong> {props.diet}<br/>
+                        <strong>Calories Desired:</strong> {props.calories}<br/>
+                    </center>
+                    <hr/>
+
+                    <strong>Breakfast:</strong> <br/>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <a href={json_result.breakfast.url} target="_blank"><img className="borderImage" src={json_result.breakfast.image}></img></a>
+                            </div>
+
+                            <div className="col-md-8 mealPadding">
+                                <strong>Item:</strong> <a href={json_result.breakfast.url} target="_blank">{json_result.breakfast.title}</a><br/>
+                                <strong>Calories:</strong> {json_result.breakfast.calories}<br/>
+                                <strong>Fat:</strong> {json_result.breakfast.fat}g<br/>
+                                <strong>Carbs:</strong> {json_result.breakfast.carbs}g<br/>
+                                <strong>Protein:</strong> {json_result.breakfast.protein}g<br/>
+                                <br/>
+                            </div>
+                        </div>
+                    
+                    <strong>Lunch:</strong> <br/>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <a href={json_result.lunch.url} target="_blank"><img className="borderImage" src={json_result.lunch.image}></img></a>
+                            </div>
+
+                            <div className="col-md-10 mealPadding">
+                            <strong>Item:</strong> <a href={json_result.lunch.url} target="_blank">{json_result.lunch.title}</a><br/>
+                                <strong>Calories:</strong> {json_result.lunch.calories}<br/>
+                                <strong>Fat:</strong> {json_result.lunch.fat}g<br/>
+                                <strong>Carbs:</strong> {json_result.lunch.carbs}g<br/>
+                                <strong>Protein:</strong> {json_result.lunch.protein}g<br/>
+                                <br/>
+                            </div>
+                        </div>
+
+                    <strong>Dinner:</strong> <br/>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <a href={json_result.dinner.url} target="_blank"><img className="borderImage" src={json_result.dinner.image}></img></a>
+                            </div>
+
+                            <div className="col-md-10 mealPadding">
+                            <strong>Item:</strong> <a href={json_result.dinner.url} target="_blank">{json_result.dinner.title}</a><br/>
+                                <strong>Calories:</strong> {json_result.dinner.calories}<br/>
+                                <strong>Fat:</strong> {json_result.dinner.fat}g<br/>
+                                <strong>Carbs:</strong> {json_result.dinner.carbs}g<br/>
+                                <strong>Protein:</strong> {json_result.dinner.protein}g<br/>
+                                <br/>
+                            </div>
+                        </div>
+
+                    <strong>Snack:</strong> <br/>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <a href={json_result.snack.url} target="_blank"><img className="borderImage" src={json_result.snack.image}></img></a>
+                            </div>
+
+                            <div className="col-md-10 mealPadding">
+                            <strong>Item:</strong> <a href={json_result.snack.url} target="_blank">{json_result.snack.title}</a><br/>
+                                <strong>Calories:</strong> {json_result.snack.calories}<br/>
+                                <strong>Fat:</strong> {json_result.snack.fat}g<br/>
+                                <strong>Carbs:</strong> {json_result.snack.carbs}g<br/>
+                                <strong>Protein:</strong> {json_result.snack.protein}g<br/>
+                                <br/>
+                            </div>
+                        </div>
+
+                        <hr/>
+                        <center>
+                            <strong>Total Calories:</strong> {json_result.total.calories}<br/>
+                            <strong>Total Fat:</strong> {json_result.total.fat}g<br/>
+                            <strong>Total Carbs:</strong> {json_result.total.carbs}g<br/>
+                            <strong>Total Protein:</strong> {json_result.total.protein}g<br/>
+                        </center>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div className="col-md-1">
+        </div>
+    </div>
+    );
+
+}
+
+export default Card;
