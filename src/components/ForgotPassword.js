f9b390db21ac20f892266eee27990d71bfadef38
David-Ibarra <DavidJewNose@gmail.com>
2020-04-04 14:48:05 -0700

    ForgotPassword.js inaccessible if logged in (test)

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import LoadingOverlay from 'react-loading-overlay'
 import {forgotPass} from './UserFunctions'
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'
+import FourOhFour from './404'
 
 class ForgotPassword extends Component {
   constructor() {
@@ -51,56 +52,65 @@ class ForgotPassword extends Component {
   }
 
   render() {
-
-    return (
-      <>
-      <LoadingOverlay
-      active={this.state.loading}
-      spinner
-      text='Sending email confirmation'
-      >
-      <div className="container">
+    if(!(localStorage.usertoken))
+    {
+      return (
+        <>
+        <LoadingOverlay
+        active={this.state.loading}
+        spinner
+        text='Sending email confirmation'
+        >
+        <div className="container">
+            
           
-        
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
+          <div className="row">
+            <div className="col-md-6 mt-5 mx-auto">
 
-            <p style={{color: 'red'}}>{this.state.errors}</p>
+              <p style={{color: 'red'}}>{this.state.errors}</p>
 
-            <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Complete fields to finish updating account credentials</h1>
-              <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  placeholder="Enter email"
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-              </div>
-          
+              <form noValidate onSubmit={this.onSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal">Complete fields to finish updating account credentials</h1>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+            
 
 
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-              >
-                Reset my password
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  className="btn btn-lg btn-primary btn-block"
+                >
+                  Reset my password
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-        <br/>
-        <br/>
-        <br/>
+          <br/>
+          <br/>
+          <br/>
 
-        
-      </div>
-      </LoadingOverlay>
-      </>
-    )
+          
+        </div>
+        </LoadingOverlay>
+        </>
+      )
+    }
+    else{
+      return(
+        <>
+          <FourOhFour/>
+        </>
+      )
+    }
   }
 }
 
