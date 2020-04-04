1f6f4223bb42d5514bef9bc3b387b24ec2b61f0d
David-Ibarra <DavidJewNose@gmail.com>
2020-04-04 14:53:06 -0700

    ResetPassword.js inaccessible if logged in

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -59,89 +59,98 @@ class ResetPassword extends Component {
   }
 
   render() {
-
-    return (
-      <>
-      <LoadingOverlay
-      active={this.state.loading}
-      spinner
-      text='Resetting Password'
-      >
-      <div className="container">
+    if(!(localStorage.usertoken))
+    {
+      return (
+        <>
+        <LoadingOverlay
+        active={this.state.loading}
+        spinner
+        text='Resetting Password'
+        >
+        <div className="container">
+            
           
-        
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
+          <div className="row">
+            <div className="col-md-6 mt-5 mx-auto">
 
-            <p style={{color: 'red'}}>{this.state.errors}</p>
+              <p style={{color: 'red'}}>{this.state.errors}</p>
 
-            <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Enter New Credentials </h1>
-              <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  placeholder="Enter email"
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="reset_key">reset key</label>
-                <input
-                  type="reset_key"
-                  className="form-control"
-                  name="reset_key"
-                  placeholder="Enter reset key"
-                  value={this.state.reset_key}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">New Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Enter Password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="confirmed_password">Confirm New Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="confirmed_password"
-                  placeholder="Enter Password Again"
-                  value={this.state.confirmed_password}
-                  onChange={this.onChange}
-                />
-              </div>
-          
+              <form noValidate onSubmit={this.onSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal">Enter New Credentials </h1>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="reset_key">reset key</label>
+                  <input
+                    type="reset_key"
+                    className="form-control"
+                    name="reset_key"
+                    placeholder="Enter reset key"
+                    value={this.state.reset_key}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">New Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Enter Password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmed_password">Confirm New Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="confirmed_password"
+                    placeholder="Enter Password Again"
+                    value={this.state.confirmed_password}
+                    onChange={this.onChange}
+                  />
+                </div>
+            
 
 
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-              >
-                Change Password
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  className="btn btn-lg btn-primary btn-block"
+                >
+                  Change Password
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-        <br/>
-        <br/>
-        <br/>
+          <br/>
+          <br/>
+          <br/>
 
-        
-      </div>
-      </LoadingOverlay>
-      </>
-    )
+          
+        </div>
+        </LoadingOverlay>
+        </>
+      )
+    }
+    else{
+      return(
+        <>
+          <FourOhFour/>
+        </>
+      )
+    }
   }
 }
 
