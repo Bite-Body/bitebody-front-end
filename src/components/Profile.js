e8449a0621e8bcd8495f4de29a3cc3b6904c381a
Bryan-Rojas <bryanrojascs@gmail.com>
2020-04-07 16:10:06 -0700

    Fixed profile WIP

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,6 +8,7 @@ class Profile extends Component {
   constructor() {
     super()
     this.state = {
+        username: '',
         first_name: '',
         last_name: '',
         email: '',
@@ -24,7 +25,8 @@ class Profile extends Component {
         first_name: decoded.identity.first_name,
         last_name: decoded.identity.last_name,
         email: decoded.identity.email,
-        id: decoded.identity.id
+        id: decoded.identity.id,
+        username: decoded.identity.username
       })
     }
     catch(error){}
@@ -50,21 +52,12 @@ class Profile extends Component {
                     </div>
                     <div class="col-md-6">
                         <div class="profile-head">
-                                    <h5>
-                                        {this.state.first_name} {this.state.last_name}
-                                    </h5>
-                                    <h6>
-                                        Collaborator
-                                    </h6>
-                                    <p class="proile-rating">RANKINGS : <span>8/10</span></p>
-                            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
-                                </li>
-                            </ul>
+                            <h5>
+                                {this.state.username}
+                            </h5>
+                            <h6>
+                                Collaborator
+                            </h6>
                         </div>
                     </div>
                     <div class="col-md-2">
@@ -72,114 +65,38 @@ class Profile extends Component {
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-work">
-                            <p>WORK LINK</p>
-                            <a href="bitebody.xyz">Bitebody.xyz</a><br/>
-                            <a href="">Bootsnipp Profile</a><br/>
-                            <a href="">Bootply Profile</a>
-                            <p>SKILLS</p>
-                            <a href="">Web Designer</a><br/>
-                            <a href="">Web Developer</a><br/>
-                            <a href="">WordPress</a><br/>
-                            <a href="">WooCommerce</a><br/>
-                            <a href="">PHP, .Net</a><br/>
-                        </div>
-                    </div>
-                    <div class="col-md-8" >
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10" >
                         <div class="tab-content profile-tab" id="myTabContent">
                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label id = "user">User Id</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>{this.state.id}</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Name</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>{this.state.first_name} {this.state.last_name}</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>{this.state.email}</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Phone</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>(310) 555-5555</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Profession</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>Web Developer and Designer</p>
-                                            </div>
-                                        </div>
-                            </div>
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Experience</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>Expert</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Hourly Rate</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>10$/hr</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Total Projects</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>230</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>English Level</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>Expert</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Availability</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>6 months</p>
-                                            </div>
-                                        </div>
                                 <div class="row">
-                                    <div class="col-md-12">
-                                        <label>Your Bio</label><br/>
-                                        <p>Your detail description</p>
+                                    <div class="col-md-2">
+                                        <label id = "user">User ID:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p>{this.state.id}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <label>Name:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p>{this.state.first_name} {this.state.last_name}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <label>Email:</label>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p>{this.state.email}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div class="col-md-1"></div>
                 </div>
             </form>           
         </div>
