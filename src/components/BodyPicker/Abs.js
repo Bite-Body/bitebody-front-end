12fdef37d5a479ee274623919caec0eeee046a2b
Bryan-Rojas <bryanrojascs@gmail.com>
2020-04-19 18:42:20 -0700

    Updated Abs page with example of data fetching

--- src/components/BodyPicker/Abs.js
+++ src/components/BodyPicker/Abs.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 
 class Abs extends Component {
-  
-  componentDidMount() {
-    document.title = "Bitebody - BodyPicker - Abs"
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+
+  async componentDidMount() {
+    document.title = "Bitebody - BodyPicker - Abs";
+    // GET request using fetch with async/await
+    const response = await fetch('https://gentle-inlet-25364.herokuapp.com/workouts/abs');
+    const data = await response.json();
+    this.setState({data: data['workouts']})
+    console.log(this.state);
   }
 
   render() {
+    const { data } = this.state;
+
     return (
 
         <div className="container">
-          Abs stuff here
+          <h1><center>Pick Your Body Part - Abs</center></h1>
+
+          {data.map(data =>
+          <>
+            <li>
+          <strong>{data.workout_name} ({data.main_muscle_group})</strong>: {data.exercise_steps}
+            </li>
+            <br/>
+          </>
+          )}
+
         </div>
 
     )
