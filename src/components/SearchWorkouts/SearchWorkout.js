e1c4856493e7497c9ed64a09011528e1c44821a3
Bryan-Rojas <bryanrojascs@gmail.com>
2020-05-12 19:46:33 -0700

    Updated search workout

--- src/components/SearchWorkouts/SearchWorkout.js
+++ src/components/SearchWorkouts/SearchWorkout.js
@@ -7,7 +7,7 @@ class SearchWorkout extends Component {
         <div class="container">
           <h1><center>Workout Search</center></h1>
           <br/>
-          <iframe title="airtable-embed" class="airtable-embed" src="https://airtable.com/embed/shrNxtFTSfjuBKjH2?backgroundColor=blue&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe>
+          <iframe title="airtable-embed" class="airtable-embed" src="https://airtable.com/embed/shrikqRv1TmB18B7o?backgroundColor=pink&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe>
         </div>
       )
     }
